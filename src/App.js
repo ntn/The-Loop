bcb680618a49ed354235bf43597c673e086affbf
ntn <neethan.bala@gmail.com>
2020-05-09 15:28:50 -0400

    Add socket.io client to retrieve stream of tweets

--- src/App.js
+++ src/App.js
@@ -2,6 +2,9 @@ import React, { useEffect, useState } from 'react';
 import TrendingList from './components/TrendingList';
 import Header from './components/Header';
 import twitterService from './services/twitter';
+import socketIOClient from "socket.io-client";
+const ENDPOINT = "http://localhost:3003";
+
 
 const App = () => {
   const [trendingTags, setTrending] = useState([]);
@@ -9,6 +12,15 @@ const App = () => {
   useEffect( () => {
     twitterService.getAll().then((initialTrendingTags) => {
       setTrending(initialTrendingTags);
+    });
+
+    const socket = socketIOClient(ENDPOINT);
+    socket.on("initialTweets", data => {
+      console.log("initialTweets: ", data);
+    });
+
+    socket.on("newTweet", data => {
+      console.log("newTweet: ", data);
     });
   }, []);
 
