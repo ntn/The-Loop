b6516d35fe9ba0a2546aba111817aaae0b2be574
ntn <neethan.bala@gmail.com>
2020-05-10 15:21:29 -0400

    Add separate components for each trending tag, add routes to obtain tweets

--- src/components/TrendingList.js
+++ src/components/TrendingList.js
@@ -1,59 +1,21 @@
 import React from 'react';
-import { makeStyles } from '@material-ui/core/styles';
-import Card from '@material-ui/core/Card';
-import CardActions from '@material-ui/core/CardActions';
-import CardContent from '@material-ui/core/CardContent';
-import Button from '@material-ui/core/Button';
-import Typography from '@material-ui/core/Typography';
-import Container from '@material-ui/core/Container';
-
-
-const useStyles = makeStyles({
-  root: {
-    minWidth: 300,
-    maxWidth: 300
-  },
-  title: {
-    fontSize: 14,
-  },
-  pos: {
-    marginBottom: 12,
-  },
-});
-
+import TrendingTagView from './TrendingTagView';
 
 const TrendingList = props => {
-  const classes = useStyles();
   const trendingTags = () => {
-    return props.trending;
+    const sanitizedTrendingTags = props.trending.map((trendingTag) => {
+      const name = trendingTag.name[0] === '#'
+        ? trendingTag.name
+        :`#${trendingTag.name.replace(/\s/g, '')}`;
+      return { ...trendingTag, name };
+    });
+    return sanitizedTrendingTags;
   };
 
   return (
     <div style={{ display: 'flex', flexWrap: 'wrap', alignItems: 'center', justifyContent: 'center' }}>
       {trendingTags().map(trendingTag =>
-        <div key={trendingTag.name}>
-          <Container maxWidth="sm">
-            <Card className={classes.root} variant="outlined">
-              <CardContent>
-                <Typography className={classes.title} color="textSecondary" gutterBottom>
-                Trending
-                </Typography>
-                <Typography variant="h5" component="h2">
-                  {trendingTag.name[0] === '#' ?
-                    trendingTag.name :
-                    `#${trendingTag.name.replace(/\s/g, '')}`}
-                </Typography>
-                <Typography className={classes.pos} color="textSecondary">
-                has {trendingTag.tweet_volume} tweets
-                </Typography>
-              </CardContent>
-              <CardActions>
-                <Button size="large">See Tweets</Button>
-              </CardActions>
-            </Card>
-            <br></br>
-          </Container>
-        </div>
+        <TrendingTagView key={trendingTag.name} name={trendingTag.name} tweetVolume={trendingTag.tweet_volume} />
       )}
     </div>
   );
---
+++ src/components/TrendingTagView.js
@@ -0,0 +1,59 @@
+import React from 'react';
+import { makeStyles } from '@material-ui/core/styles';
+import Card from '@material-ui/core/Card';
+import CardActions from '@material-ui/core/CardActions';
+import CardContent from '@material-ui/core/CardContent';
+import Button from '@material-ui/core/Button';
+import Typography from '@material-ui/core/Typography';
+import Container from '@material-ui/core/Container';
+
+import {
+  BrowserRouter as Router,
+  Link,
+} from 'react-router-dom';
+
+
+const useStyles = makeStyles({
+  root: {
+    minWidth: 300,
+    maxWidth: 300
+  },
+  title: {
+    fontSize: 14,
+  },
+  pos: {
+    marginBottom: 12,
+  },
+});
+
+const TrendingTagView = ({ name, tweetVolume }) => {
+  const classes = useStyles();
+
+  return (
+    <div key={name}>
+      <Container maxWidth="sm">
+        <Card className={classes.root} variant="outlined">
+          <CardContent>
+            <Typography className={classes.title} color="textSecondary" gutterBottom>
+            Trending
+            </Typography>
+            <Typography variant="h5" component="h2">
+              {name}
+            </Typography>
+            <Typography className={classes.pos} color="textSecondary">
+            has {tweetVolume} tweets
+            </Typography>
+          </CardContent>
+          <CardActions>
+            <Button size="large">
+              <Link to={`/tweets/${name.substr(1)}`}>See Tweets</Link>
+            </Button>
+          </CardActions>
+        </Card>
+        <br></br>
+      </Container>
+    </div>
+  );
+};
+
+export default TrendingTagView;
