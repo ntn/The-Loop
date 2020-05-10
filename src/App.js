57075b05c3578ceba18b414e8260a55e66429ce6
ntn <neethan.bala@gmail.com>
2020-05-10 19:32:18 -0400

    Add routing to main view

--- src/App.js
+++ src/App.js
@@ -1,24 +1,45 @@
 import React, { useEffect, useState } from 'react';
 import TrendingList from './components/TrendingList';
 import Header from './components/Header';
-import RealTimeTweetsList from './components/RealTimeTweetsList';
+import TweetList from './components/TweetList';
+import RealtimeTweetList from './components/RealtimeTweetList';
 import twitterService from './services/twitter';
-
+import {
+  BrowserRouter as Router,
+  Switch,
+  Route,
+  useRouteMatch
+} from "react-router-dom"
 
 const App = () => {
   const [trendingTags, setTrending] = useState([]);
+  // const [tweetsForTag, setTweetsForTag] = useState(thing);
 
   useEffect( () => {
-    twitterService.getAll().then((initialTrendingTags) => {
+    twitterService.getAllTrendingTags().then((initialTrendingTags) => {
       setTrending(initialTrendingTags);
     });
   }, []);
 
+  const match = useRouteMatch('/tweets/:searchQuery');
+  const tagName = match
+    ? match.params.searchQuery
+    : null;
+
   return (
     <>
       <Header title={'The Loop'} />
-      <TrendingList trending={trendingTags} />
-      <RealTimeTweetsList />
+      <Switch>
+        <Route path="/realtime/coronavirus">
+          <RealtimeTweetList />
+        </Route>
+        <Route path="/tweets/:searchQuery">
+          <TweetList tagName={tagName} />
+        </Route>
+        <Route path="/">
+          <TrendingList trending={trendingTags} />
+        </Route>
+      </Switch>
     </>
   );
 };
