7d990e4dcab35ddf837d494854595936ad1f1170
ntn <neethan.bala@gmail.com>
2020-05-10 00:42:57 -0400

    Retrieve list of real-time tweets for a hashtag

--- src/App.js
+++ src/App.js
@@ -1,9 +1,8 @@
 import React, { useEffect, useState } from 'react';
 import TrendingList from './components/TrendingList';
 import Header from './components/Header';
+import RealTimeTweetsList from './components/RealTimeTweetsList';
 import twitterService from './services/twitter';
-import socketIOClient from "socket.io-client";
-const ENDPOINT = "http://localhost:3003";
 
 
 const App = () => {
@@ -13,21 +12,13 @@ const App = () => {
     twitterService.getAll().then((initialTrendingTags) => {
       setTrending(initialTrendingTags);
     });
-
-    const socket = socketIOClient(ENDPOINT);
-    socket.on("initialTweets", data => {
-      console.log("initialTweets: ", data);
-    });
-
-    socket.on("newTweet", data => {
-      console.log("newTweet: ", data);
-    });
   }, []);
 
   return (
     <>
       <Header title={'The Loop'} />
       <TrendingList trending={trendingTags} />
+      <RealTimeTweetsList />
     </>
   );
 };
---
+++ src/components/RealTimeTweetsList.js
@@ -0,0 +1,44 @@
+import React, { useEffect, useState } from 'react';
+import socketIOClient from 'socket.io-client';
+import TweetView from './TweetView';
+const ENDPOINT = 'http://localhost:3003';
+
+const RealTimeTweetsList = props => {
+  const [tweets, setTweets] = useState([]);
+  const [initialized, setInitialized] = useState(false);
+
+  useEffect(() => {
+    const socket = socketIOClient(ENDPOINT);
+    socket.on('initialTweets', data => {
+      const newTweets = tweets.concat(data);
+      setTweets(newTweets);
+      setInitialized(true);
+    });
+  }, []);
+
+  useEffect(() => {
+    if (initialized) {
+      const socket = socketIOClient(ENDPOINT);
+
+      socket.on('newTweet', data => {
+        tweets.push(data.newTweet);
+        setTweets([...tweets]);
+      });
+    }
+  }, [initialized]);
+
+  return (
+    <div>
+      {[...new Set(tweets)].map(tweet =>
+        <TweetView
+          key={tweet.id}
+          name={tweet.user.screen_name}
+          profilePic={tweet.user.profile_image_url}
+          createdDate={tweet.created_at}
+          text={tweet.text} />
+      )}
+    </div>
+  );
+};
+
+export default RealTimeTweetsList;
