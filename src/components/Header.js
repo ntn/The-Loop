aaaa3deedf72bb06d6637d7b74f3f219cb540ef6
ntn <neethan.bala@gmail.com>
2020-05-10 19:32:54 -0400

    Add links to realtime tweets page and home in header

--- src/components/Header.js
+++ src/components/Header.js
@@ -4,6 +4,12 @@ import AppBar from '@material-ui/core/AppBar';
 import CssBaseline from '@material-ui/core/CssBaseline';
 import Toolbar from '@material-ui/core/Toolbar';
 import Typography from '@material-ui/core/Typography';
+import Badge from '@material-ui/core/Badge';
+import Button from '@material-ui/core/Button';
+import {
+  BrowserRouter as Router,
+  Link,
+} from 'react-router-dom';
 
 const useStyles = makeStyles(() => ({
   root: {
@@ -15,17 +21,30 @@ const useStyles = makeStyles(() => ({
   },
 }));
 
+
 const Header = ({ title }) => {
   const classes = useStyles();
+  const linkStyle = {
+    color: 'inherit',
+    textDecoration: 'inherit'
+  };
 
   return (
     <div className={classes.root}>
       <CssBaseline />
       <AppBar position="relative">
         <Toolbar>
+          <Button color="inherit">
+            <Link to="/" style={linkStyle}>Home</Link>
+          </Button>
           <Typography variant="h6" className={classes.title}>
             {title}
           </Typography>
+          <Badge color="secondary" variant="dot">
+            <Button color="inherit">
+              <Link to="/realtime/coronavirus" style={linkStyle}>COVID-19 Tweets</Link>
+            </Button>
+          </Badge>
         </Toolbar>
       </AppBar>
       <br></br>
