a28cf73c445cbc6cf2b7b5e1d973907829cb38ef
ntn <neethan.bala@gmail.com>
2020-05-10 15:23:23 -0400

    Update route for obtaining tags for a hashtag

--- src/services/twitter.js
+++ src/services/twitter.js
@@ -1,9 +1,14 @@
 import axios from 'axios';
 const baseUrl = 'http://localhost:3003/twitter';
 
-const getAll = () => {
+const getAllTrendingTags = () => {
   const request = axios.get(`${baseUrl}/trending`);
   return request.then(response => response.data);
 };
 
-export default { getAll };
+const getTweetsByTag = (topic) => {
+  const request = axios.get(`${baseUrl}/tweets/${topic}`);
+  return request.then(response => response.data);
+};
+
+export default { getAllTrendingTags, getTweetsByTag };
