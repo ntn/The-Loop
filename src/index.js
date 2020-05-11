71aa1bb12a16514008b02e1d6653c3dc7c921936
ntn <neethan.bala@gmail.com>
2020-05-10 22:49:07 -0400

    Add redux to application

--- src/index.js
+++ src/index.js
@@ -1,13 +1,17 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import App from './App';
+import store from './store';
+import { Provider } from 'react-redux';
 import {
   BrowserRouter as Router
 } from 'react-router-dom';
+import App from './App';
 
 ReactDOM.render(
-  <Router>
-    <App />
-  </Router>,
+  <Provider store={store}>
+    <Router>
+      <App />
+    </Router>
+  </Provider>,
   document.getElementById('root')
 );
